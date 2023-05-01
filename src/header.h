210dac9c7dc2f420cac012d0592d107307cfa09d
kuvaus <22169537+kuvaus@users.noreply.github.com>
2023-05-01 11:11:31 +0000

    Undo windows file path difference

--- src/header.h
+++ src/header.h
@@ -51,9 +51,6 @@ struct LLMParams {
 
     int32_t n_batch = 9;
     std::string model = "./models/ggml-vicuna-13b-1.1-q4_2.bin";
-    #ifdef _WIN32
-        model = ".\\models\\ggml-vicuna-13b-1.1-q4_2.bin";
-    #endif
 };
 
 enum ConsoleColor {
