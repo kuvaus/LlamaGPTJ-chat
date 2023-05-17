af39eaf973f831ea7f4d36b567674c02f130b0c8
kuvaus <kuvaus@users.noreply.github.com>
2023-05-17 16:55:22 +0300

    AVX512 option

--- src/utils.h
+++ src/utils.h
@@ -18,7 +18,7 @@ void check_avx_support_at_startup() {
 #if defined(__x86_64__) || defined(__i386__)
     const bool avx(__builtin_cpu_supports("avx"));
     const bool avx2(__builtin_cpu_supports("avx2"));
-    const bool avx512(__builtin_cpu_supports("avx512"));
+    const bool avx512(__builtin_cpu_supports("avx512f"));
     const bool fma(__builtin_cpu_supports("fma"));
     if (avx512 && avx && avx2 && fma) {std::cout << "Your computer supports AVX512" << std::endl;}
     else if (avx && avx2 && fma)      {std::cout << "Your computer supports AVX2" << std::endl;}
